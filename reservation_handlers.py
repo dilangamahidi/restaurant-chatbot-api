87eef35c57f78c4ce8f44b85899b8cd321ba651c
Ajeje-brazzorf <samuele.0.alberti@gmail.com>
2025-06-23 17:15:08 +0200

    Update reservation_handlers.py

--- reservation_handlers.py
+++ reservation_handlers.py
@@ -624,7 +624,7 @@ def handle_modify_reservation(parameters):
                     },
                     {
                         "text": {
-                            "text": ["You can say:\n• 'Change the date to tomorrow'\n• 'Change the time to 8pm'\n• 'Change to 4 guests'\n• Or call us for complex changes"]
+                            "text": ["You can say: 'Change the date to tomorrow', 'Change the time to 8pm', 'Change to 4 guests'"]
                         }
                     }
                 ]
