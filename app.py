4f9eee846d7e1c1dbbd0d281207f0ab9ca08d539
dilangamahidi <dilangamahidi@gmail.com>
2025-06-26 15:43:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """
-Main Flask application for restaurant - Modularized and reduced version
+Main Flask application for restaurant
 """
 from flask import Flask, request, jsonify
 from flask_cors import CORS
